40e86b2673cac9a106ed263c00bd62ccbae0df25
1amankwah <stephen.amankwah@meteo.gov.gh>
2024-11-29 23:57:11 +0000

    refact

--- src/components/FarmerToolBox.jsx
+++ src/components/FarmerToolBox.jsx
@@ -471,7 +471,7 @@ const FarmerToolBox = ({ showBreadCrumb = true }) => {
       </div>
 
       {/* Tools Grid */}
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6">
         {tools.map((tool) => (
           <div
             key={tool.id}
